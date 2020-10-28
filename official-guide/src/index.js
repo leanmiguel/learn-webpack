84a94826b73f464b730bad6d8b0f269ac63df593
Lean <theleanmiguel@gmail.com>
2020-10-28 08:31:13 -0600

    reset for output management

--- official-guide/src/index.js
+++ official-guide/src/index.js
@@ -1,33 +1,9 @@
-import "./style.css";
-import Icon from "./heart.png";
+import _ from "lodash";
 
-import Data from "./data.xml";
-import Notes from "./data.csv";
-
-const component = () => {
+function component() {
   const element = document.createElement("div");
-  // element.innerHTML = _.join(["hello", "webpack"], " ");
-  element.innerHTML = "yeah";
-  element.classList.add("hello");
-
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
   return element;
-};
+}
 
 document.body.appendChild(component());
-
-import toml from "./data.toml";
-import yaml from "./data.yaml";
-import json from "./data.json5";
-
-console.log(toml.title); // output `TOML Example`
-console.log(toml.owner.name); // output `Tom Preston-Werner`
-
-console.log(yaml.title); // output `YAML Example`
-console.log(yaml.owner.name); // output `Tom Preston-Werner`
-
-console.log(json.title); // output `JSON5 Example`
-console.log(json.owner.name); // output `Tom Preston-Werner`
--- official-guide/webpack.config.js
+++ official-guide/webpack.config.js
@@ -1,65 +1,9 @@
 const path = require("path");
-const toml = require("toml");
-const yaml = require("yamljs");
-const json5 = require("json5");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader"],
-      },
-      {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-              outputPath: "fonts/",
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(csv|tsv)$/,
-        use: ["csv-loader"],
-      },
-      {
-        test: /\.xml$/,
-        use: ["xml-loader"],
-      },
-      {
-        test: /\.toml$/,
-        type: "json",
-        parser: {
-          parse: toml.parse,
-        },
-      },
-      {
-        test: /\.yaml$/,
-        type: "json",
-        parser: {
-          parse: yaml.parse,
-        },
-      },
-      {
-        test: /\.json5$/,
-        type: "json",
-        parser: {
-          parse: json5.parse,
-        },
-      },
-    ],
   },
 };
