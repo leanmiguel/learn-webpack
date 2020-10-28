b5367cee1639319dc12a2e740aef00c056bca94f
Lean <theleanmiguel@gmail.com>
2020-10-28 08:29:33 -0600

    finish asset management

--- official-guide/src/index.js
+++ official-guide/src/index.js
@@ -1,10 +1,13 @@
-import _ from "lodash";
 import "./style.css";
 import Icon from "./heart.png";
 
+import Data from "./data.xml";
+import Notes from "./data.csv";
+
 const component = () => {
   const element = document.createElement("div");
-  element.innerHTML = _.join(["hello", "webpack"], " ");
+  // element.innerHTML = _.join(["hello", "webpack"], " ");
+  element.innerHTML = "yeah";
   element.classList.add("hello");
 
   const myIcon = new Image();
@@ -15,3 +18,16 @@ const component = () => {
 };
 
 document.body.appendChild(component());
+
+import toml from "./data.toml";
+import yaml from "./data.yaml";
+import json from "./data.json5";
+
+console.log(toml.title); // output `TOML Example`
+console.log(toml.owner.name); // output `Tom Preston-Werner`
+
+console.log(yaml.title); // output `YAML Example`
+console.log(yaml.owner.name); // output `Tom Preston-Werner`
+
+console.log(json.title); // output `JSON5 Example`
+console.log(json.owner.name); // output `Tom Preston-Werner`
--- official-guide/webpack.config.js
+++ official-guide/webpack.config.js
@@ -1,4 +1,7 @@
 const path = require("path");
+const toml = require("toml");
+const yaml = require("yamljs");
+const json5 = require("json5");
 
 module.exports = {
   entry: "./src/index.js",
@@ -15,6 +18,47 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: ["file-loader"],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: ["csv-loader"],
+      },
+      {
+        test: /\.xml$/,
+        use: ["xml-loader"],
+      },
+      {
+        test: /\.toml$/,
+        type: "json",
+        parser: {
+          parse: toml.parse,
+        },
+      },
+      {
+        test: /\.yaml$/,
+        type: "json",
+        parser: {
+          parse: yaml.parse,
+        },
+      },
+      {
+        test: /\.json5$/,
+        type: "json",
+        parser: {
+          parse: json5.parse,
+        },
       },
     ],
   },
